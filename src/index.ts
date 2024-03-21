772e13176b988c322661134bf5e5b403e2b40eb0
Sayam Alvi <sayamalvi07@gmail.com>
2024-03-21 14:35:06 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-// const client = new Redis(process.env.REDIS_URL!);
-const client = createClient({ url: process.env.REDIS_URL });
+
 app.use(cors());
 app.use(express.json());
 
+const client = createClient({ url: process.env.REDIS_URL });
 app.post("/submit", async (req, res) => {
   const { username, code, language, input } = await req.body;
 
@@ -75,18 +75,18 @@ app.post("/submit", async (req, res) => {
   }
 });
 app.get("/submissions", async (req, res) => {
-  const cachedData = await client.get("submissions");
+  // const cachedData = await client.get("submissions");
 
-  if (cachedData) {
-    console.log("Data retrieved from Redis cache");
-    return res.json(JSON.parse(cachedData));
-  }
+  // if (cachedData) {
+  //   console.log("Data retrieved from Redis cache");
+  //   return res.json(JSON.parse(cachedData));
+  // }
 
   const q = await db.query("SELECT * FROM submissions");
   console.log("Data retrieved from database");
 
-  await client.set("submissions", JSON.stringify(q[0]));
-  await client.expire("submissions", 20);
+  // await client.set("submissions", JSON.stringify(q[0]));
+  // await client.expire("submissions", 20);
 
   return res.json(q[0]);
 });
